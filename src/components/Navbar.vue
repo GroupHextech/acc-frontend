b8f22457d3e65aafdbc2eb7e9769f7e995d436fe
michelrubens <michelrube@gmail.com>
2023-05-21 16:41:31 -0300

    Frontend Corrections pt 2 (Navbar.vue)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,85 @@
 <template>
   <div>
-    <nav class="navbar is-link is-fixed-top" role="navigation" aria-label="main navigation">
+    <nav
+      class="navbar is-link is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
       <div class="navbar-brand">
         <a class="navbar-item" href="#">
-          <img alt="logo" src="../assets/sedan-car-side-black-silhouette.svg" width="70" height="28" />
+          <img
+            alt="logo"
+            src="../assets/sedan-car-side-black-silhouette.svg"
+            width="70"
+            height="28"
+          />
           EMBRACAR
         </a>
 
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"
-          v-if="isLoggedIn" @click="toggleMenuActive">
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          v-if="isLoggedIn"
+          @click="toggleMenuActive"
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': isMenuActive }" v-if="isLoggedIn">
-
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': isMenuActive }"
+        v-if="isLoggedIn"
+      >
         <div class="navbar-start">
-          <router-link class="navbar-item" to="/dashboard" @click="handleMenuButtonClick">Dashboard</router-link>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <router-link class="navbar-link" to="/chassis" @click="handleMenuButtonClick">Chassis</router-link>
+          <router-link
+            class="navbar-item"
+            to="/dashboard"
+            @click="handleMenuButtonClick"
+            >Dashboard</router-link
+          >
+          <div
+            class="navbar-item has-dropdown"
+            v-bind:class="{ 'is-hoverable': hasPermission('restrict') }"
+          >
+            <router-link
+              class="navbar-link"
+              to="/chassis"
+              @click="handleMenuButtonClick"
+              >Chassis</router-link
+            >
             <div class="navbar-dropdown">
-              <router-link class="navbar-item" to="/chassis/register" @click="handleMenuButtonClick">Add chassis</router-link>
+              <router-link
+                class="navbar-item"
+                to="/chassis/register"
+                @click="handleMenuButtonClick"
+                >Add chassis</router-link
+              >
             </div>
           </div>
-          <router-link class="navbar-item" to="/items" @click="handleMenuButtonClick">Items</router-link>
-          <a class="navbar-item" @click="handleMenuButtonClick">Service Bulletins</a>
+          <router-link
+            class="navbar-item"
+            to="/items"
+            @click="handleMenuButtonClick"
+            >Items</router-link
+          >
+          <a class="navbar-item" @click="handleMenuButtonClick"
+            >Service Bulletins</a
+          >
         </div>
 
         <div class="navbar-end" v-bind:class="{ 'is-centered': isMenuActive }">
           <div class="navbar-item is-flex">
             <div class="buttons">
-              <a class="button is-link is-light is-small" @click="logout"> Sign out </a>
+              <a class="button is-link is-light is-small" @click="logout">
+                Sign out
+              </a>
             </div>
           </div>
         </div>
@@ -53,6 +100,11 @@ export default defineComponent({
     const router = useRouter();
     const authStore = useAuthStore();
     const isMenuActive = ref(false);
+
+    function hasPermission(permission: "allowed" | "restrict"): boolean {
+      const authorization = sessionStorage.getItem("authorization");
+      return authStore.hasPermission(permission, authorization);
+    }
 
     const isLoggedIn = computed(() => {
       // Verifica se o usuário está na rota de login
@@ -85,6 +137,7 @@ export default defineComponent({
       isLoggedIn,
       isMenuActive,
       toggleMenuActive,
+      hasPermission,
       handleMenuButtonClick,
       logout,
     };
