be331791a04f78d03764734d0ad1352ad4c80968
Michel Rubens <michelrube@gmail.com>
2023-05-27 14:29:40 -0300

    Update Dashboard with ChassisContainSB component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -12,6 +12,9 @@
         <section class="hero is-small box">
           <IncorpItemsByChassi/>
         </section>
+        <section class="hero is-small box">
+          <ChassisContainsSB/>
+        </section>
       </div>
     </section>
   </div>
@@ -21,6 +24,7 @@
 import SBbyStatus from '../components/charts/SBbyStatus.vue'
 import ChassisByItem from '../components/charts/ChassisByItem.vue';
 import IncorpItemsByChassi from '../components/charts/IncorpItemsByChassi.vue';
+import ChassisContainsSB from '../components/charts/ChassisContainsSB.vue';
 </script>
 
 <style scoped>
