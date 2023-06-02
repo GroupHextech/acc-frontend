69ff21ac9a063ba0fa152c8ab87d5d2deabef39c
Michel Rubens <michelrube@gmail.com>
2023-06-01 21:09:19 -0300

    FATECHEX4-70 -> CSV from SBbyStatus and ItemsByChassi

--- src/components/charts/IncorpItemsByChassi.vue
+++ src/components/charts/IncorpItemsByChassi.vue
@@ -4,13 +4,17 @@
     <div v-if="isLoading">
       <loading />
     </div>
-    <canvas id="incorpItemsByChassiChart"></canvas>
+    <canvas class="block" id="incorpItemsByChassiChart"></canvas>
+    <button class="button is-link is-light" @click="exportCSV" v-if="!isLoading">
+      Export to CSV
+    </button>
   </div>
 </template>
 
 <script lang="js">
 import Chart from 'chart.js/auto'
 import Loading from '../Loading.vue'
+import axios from 'axios';
 
 export default {
   name: 'IncorpItemsByChassiChart',
@@ -20,41 +24,103 @@ export default {
   data() {
     return {
       isLoading: true,
+      chart: null,
     }
   },
   mounted() {
-    // Mocked data for testing
-    const mockData = {
-      labels: ['Chassi A', 'Chassi B', 'Chassi C', 'Chassi D'],
-      datasets: [{
-        label: 'Incorporated Items',
-        data: [8, 12, 6, 10],
-        backgroundColor: '#33D321',
-      }]
-    };
-
-    const config = {
-      type: 'bar',
-      data: mockData,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
+    const authToken = sessionStorage.getItem("authToken");
+    const configToken = {
+      headers: {
+        authorization: authToken,
       },
     };
     const ctx = document.getElementById('incorpItemsByChassiChart');
-    const chart = new Chart(ctx, config);
-    this.isLoading = false;
+    axios.get('/item/contagem', configToken)
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+
+        const chartData = {
+          labels: data.map(item => item.chassi),
+          datasets: [{
+            label: 'Incorporated Items',
+            data: data.map(item => item.count_item),
+            backgroundColor: '#33D321',
+          }]
+        };
+
+        const config = {
+          type: 'bar',
+          data: chartData,
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          },
+        };
+
+        const chart = new Chart(ctx, config);
+        this.chart = chart;
+        this.isLoading = false;
+      })
+      .catch(error => {
+        console.error(error);
+        this.isLoading = false;
+      });
+  },
+  methods: {
+    exportCSV() {
+      // Obtém os dados do gráfico
+      const chartData = this.chart.config.data;
+
+      // Cria uma matriz para armazenar os dados do CSV
+      const csvData = [];
+
+      // Adiciona o cabeçalho do CSV
+      const headers = ['Chassi', 'Incorporated Items'];
+      csvData.push(headers);
+
+      // Adiciona cada linha de dados do gráfico ao CSV
+      for (let i = 0; i < chartData.labels.length; i++) {
+        const label = chartData.labels[i];
+        const value = chartData.datasets[0].data[i];
+        csvData.push([label, value.toString()]);
+      }
+
+      // Cria o conteúdo do arquivo CSV
+      let csvContent = 'data:text/csv;charset=utf-8,';
+      csvData.forEach(row => {
+        const csvRow = row.map(value => `"${value}"`).join(';');
+        csvContent += csvRow + '\r\n';
+      });
+
+      // Cria um elemento de link para fazer o download do arquivo CSV
+      const link = document.createElement('a');
+      link.setAttribute('href', encodeURI(csvContent));
+      const currentDate = new Date().toISOString().split('T')[0];
+      const fileName = `Items_by_Chassi_${currentDate}.csv`;
+      link.setAttribute('download', fileName);
+      document.body.appendChild(link);
+
+      // Simula um clique no link para iniciar o download do arquivo CSV
+      link.click();
+
+      // Remove o elemento de link
+      document.body.removeChild(link);
+    }
   }
 }
 </script>
 
 <style scoped>
 .chart-container {
-  max-width: 600px; /* Defina a largura máxima do contêiner do gráfico */
-  width: 100%; /* Ocupa todo o espaço disponível */
-  margin: 0 auto; /* Centraliza o contêiner horizontalmente */
+  max-width: 600px;
+  /* Defina a largura máxima do contêiner do gráfico */
+  width: 100%;
+  /* Ocupa todo o espaço disponível */
+  margin: 0 auto;
+  /* Centraliza o contêiner horizontalmente */
 }
 </style>
